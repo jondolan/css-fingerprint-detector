e9544defc910b1311c6a6a7477c6055d37103d39
Jon Dolan <jon@jondolan.io>
2025-04-30 16:31:02 -0400

    improved calc detector

--- tests/test_complex_calc_detector.py
+++ tests/test_complex_calc_detector.py
@@ -57,3 +57,94 @@ def test_no_complex_calc_detected():
     """
     results = complex_calc_detector(css)
     assert len(results) == 0
+
+def test_poc_chrome():
+    css = """
+    @container calccontainer (width: 0px) {
+        #calcUbuntu {
+            color: orangered;
+        }
+        #calcWindows {
+            display: none;
+        }
+      }
+      @container calccontainer (width: 1.84375px) {
+        #calcUbuntu {
+            display: none;
+        }
+        #calcWindows {
+            color: blue;
+        }
+      }
+    """
+    results = complex_calc_detector(css)
+    assert len(results) == 0
+
+def test_office_detection():
+    css = """
+    #testcontainer {
+        container-type: inline-size;
+        font-family: 'Gill Sans MT', 'Gill Sans';
+        font-style: normal;
+        font-weight: 100;
+        font-size: 11px;
+        width: 1cap;
+    }
+    @container (max-width: 7.5px) {
+        p {
+            color: green;
+        }
+        #no {
+            display: none;
+        }
+        #yes {
+            display: block;
+        }
+    }
+    """
+    results = complex_calc_detector(css)
+    assert len(results) == 0
+
+def test_poc_firefox():
+    css = """
+    .wrapper {
+        width: fit-content;
+      }
+      #containerCalc {
+        container: calccontainer / inline-size;
+      }
+      #targetCalc {
+        width: calc(
+          1px *
+            (
+               (  e  * 0.06314882636070251 - 0.06699182000011206 /  ( 
+              327510.10546596383 * 101099.74005273856 )  )  + 
+              0.9363944577053189 /  sin(  sin( 86911.80023335948 *  tan( 
+              122224.59393033749 )  /  tan( 250486.18265094055 )  +  ( 
+              169617.27745474092 )  /  pi  * 19.00493122072233 - 
+              0.22360279853455722 )  / 50590.01594434995 +  tan(  ( 
+              110958.53977223029 )  + 109345.15143883083 * 99223.79864865377 
+              + 0.05425928323529661 )  / 94812.65262083427 *  pi  )  - 
+              0.8964629967231303 * -341499.34226304095
+            )
+        );
+      }
+      @container calccontainer (width: 293874.6875px) {
+        #calcUbuntu {
+          color: orangered;
+        }
+        #calcWindows {
+          display: none;
+        }
+      }
+      @container calccontainer (width: 293694.0625px) {
+        #calcUbuntu {
+          display: none;
+        }
+        #calcWindows {
+          color: blue;
+        }
+      }
+    """
+    results = complex_calc_detector(css)
+    assert len(results) == 1
